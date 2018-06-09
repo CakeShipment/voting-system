63db15b478777ad4a7a1db8a72f8f600701c15b7
CakeShipment <thats.myboy2008@gmail.com>
2018-06-09 10:36:00 +0800

    Fixed bug

--- src/votingsystem/VotingSystem.java
+++ src/votingsystem/VotingSystem.java
@@ -8,6 +8,7 @@ package votingsystem;
 import votingsystem.model.Storage;
 import votingsystem.model.Candidate;
 
+
 /**
  *
  * @author staff
@@ -18,10 +19,10 @@ public class VotingSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Storage.addCandidate(new Candidate("Trump Donald","President",0));
-        Storage.addCandidate(new Candidate("Kasey Cuyos","Mayor",0));
-        Storage.addCandidate(new Candidate("Renz Suck","Vice President",0));
-        Storage.addCandidate(new Candidate("Dan Help","District Representative",0));
+        Storage.addCandidate(new Candidate("Trump Donald",Candidate.candType.President,0));
+        Storage.addCandidate(new Candidate("Kasey Cuyos",Candidate.candType.Mayor,0));
+        Storage.addCandidate(new Candidate("Renz Suck",Candidate.candType.Vice_President,0));
+        Storage.addCandidate(new Candidate("Dan Help",Candidate.candType.District_Representative,0));
         int candSize = Storage.getCandList().size();
         for (int i = 0; i < candSize; i++){
             System.out.println(Storage.getCandidate(i).getFullName());
